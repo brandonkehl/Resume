f80830795195172a1d8eb4e48c29cc0c9604db45
brandon kehl <brandonkehl@Mac-mini.local>
2021-10-05 18:18:51 -0500

    restarted main.js

--- Main.js
+++
@@ -1,7 +0,0 @@
-async function getVisitors(){
-const response = await fetch('https://1xdilp5rt1.execute-api.us-east-1.amazonaws.com');
-const data = await response.json();
-const {visitorID} = data;
-
-document.getElementById('visits').textContent = visitorID;
-}
