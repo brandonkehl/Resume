c1af4e8b555df25c744121703e2cf183d8fe0af8
Brandon <kehl.brandon@gmail.com>
2021-11-02 23:36:08 -0500

    added homelab link

--- lambda.py
+++ lambda.py
@@ -1,22 +1,33 @@
+
 import json
+import boto3
+import datetime
 
-def lambda_handler(event, context):
 
-    visitId = event['queryStringParameter']['visitID']
-    visitCount = event['queryStringParameter']['count']
-
-    print('visitors='+ visitCount)
-
-    # body of response
-    visitResponse ={}
-   # visitResponse['visitId'] = visitId
-    visitResponse['count'] = visitCount 
-
-    # http response
-    responseObject ={}
-    responseObject['statusCode'] = 200
-    responseObject['headers'] = {}
-    responseObject['headers']['Content-Type'] = 'application/json'
-    responseObject['body'] = json.dumps(visitResponse)
-
-    return responseObject
+def handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    client = boto3.client('dynamodb')
+    
+    count = dynamodb.Table('Visits')
+    
+    
+    
+    # Putting a try/catch to log to user when some error occurs
+    try:
+        
+        count.put_item(
+           Item={
+                'count': {'N': 'count'}
+                
+            }
+        )
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Succesfull')
+        }
+    except:
+        return {
+                'statusCode': 400,
+                'body': json.dumps('Error saving the data')
+        }
